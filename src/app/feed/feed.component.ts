2237b0f20231cbe00bc8ac6366f2f55e1ba2e383
leoglima <leoglima@gmail.com>
2021-10-26 14:39:00 -0300

    correção de pequenos bugs

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
+  
+
 }
+
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../model/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    return this.http.get('http://localhost:3000/posts')
+  }
+
+  postMensagem(post: Post) {
+    return this.http.post('http://localhost:3000/posts', post)
+  }
 }
